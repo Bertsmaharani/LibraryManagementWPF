8e4ad9b9a3f63b66d15124568e3d0406c164ee89
Клименок Дарья <dashaklimenock@gmail.com>
2025-04-23 18:06:04 +0300

    Окно авторизации

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,13 +5,45 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace LibraryManagementWPF
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
+        public static new MainWindow MainWindow { get; set; }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var authWindow = new Authentification();
+
+            if (authWindow != null)
+            {
+
+                bool? result = authWindow.ShowDialog();
+                if (result == true)
+                { 
+                    if (MainWindow == null)
+                    {
+                        MainWindow = new MainWindow(); 
+                        MainWindow.Show(); 
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Авторизация не удалась.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Shutdown();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка при создании окна авторизации.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                Shutdown();
+            }
+        }
     }
 }
---
+++ Authentification.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManagementWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Authentification.xaml
+    /// </summary>
+    public partial class Authentification : Window
+    {
+        public Authentification()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonEnterAuthorization_Click(object sender, RoutedEventArgs e)
+        {
+            string username = TextBoxLogin.Text;
+            string password = TextBoxPassword.Password;
+
+            if (IsCredentialsValid(username, password))
+            {
+                DialogResult = true; // Успешная авторизация
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                DialogResult = false; // Неуспешная авторизация
+            }
+        }
+        private bool IsCredentialsValid(string username, string password)
+        {
+            return username == "dasha" && password == "pass";
+        }
+    }
+}
